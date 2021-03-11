1a4ad067c1f0598dad396cab2a7d69804d3f0d6a
Antonio Sampaio De Vuono <antoniosvuono@gmail.com>
2021-03-11 12:46:39 -0300

    ESlint prettier e editor configurados !

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Ola B2FREE!</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Ola B2FREE!</h1>
+                <p>Antonio Sampaio De Vuono teste</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
