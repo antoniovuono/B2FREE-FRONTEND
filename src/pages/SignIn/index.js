df294b04429b46a54088d0791708d5b66c37c052
Antonio Sampaio De Vuono <antoniosvuono@gmail.com>
2021-03-15 01:17:03 -0300

    Manipulação de estados do cadastro finalizada e validacões feitas!

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Insira um email válido!')
+        .required('É necessário ter um email para logar...'),
+    password: Yup.string().required('É necessário ter uma senha para logar'),
+});
 
 export default function SignIn() {
     function handleSubmit(data) {
@@ -10,7 +18,7 @@ export default function SignIn() {
     return (
         <>
             <h1>B2FREE</h1>
-            <Form onSubmit={handleSubmit}>
+            <Form schema={schema} onSubmit={handleSubmit}>
                 <Input name="email" type="email" placeholder="E-mail" />
                 <Input name="password" type="password" placeholder="Senha" />
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é obrigatório...'),
+    cpf: Yup.number('Insira apenas os números !').required(
+        'O cpf é obrigatório...'
+    ),
+    email: Yup.string()
+        .email('Insira um email válido')
+        .required('O email é obrigatório...'),
+    password: Yup.string()
+        .min(6, 'A senha precisa de ter 6 caracteres no mínimo')
+        .required('A Senha é obrigatória'),
+});
 
 export default function SignUp() {
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+
     return (
         <>
             <h1>B2FREE</h1>
-            <form>
-                <input type="name" placeholder="Nome"></input>
-                <input type="number" placeholder="CPF/CNPJ"></input>
-                <input type="email" placeholder="E-mail"></input>
-                <input type="password" placeholder="Senha"></input>
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input name="name" type="name" placeholder="Nome" />
+                <Input name="cpf" type="number" placeholder="CPF/CNPJ" />
+                <Input name="email" type="email" placeholder="E-mail" />
+                <Input name="password" type="password" placeholder="Senha" />
 
                 <button type="submit">Cadastrar</button>
                 <Link to="/">Já é cadastrado ?</Link>
-            </form>
+            </Form>
         </>
     );
 }
