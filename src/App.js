df0058c5cfe56b4a08a0186cb19ee6d821217d54
Antonio Sampaio De Vuono <antoniosvuono@gmail.com>
2021-03-16 19:43:47 -0300

    Corrigindo bugs de loading e do refresh da páginas!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './config/ReactotronConfig';
 import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Router } from 'react-router-dom';
+import { Router, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Routes from './routes';
 import history from './services/history';
@@ -10,14 +10,16 @@ import { store, persistor } from './store';
 
 function App() {
     return (
-        <Provider store={store}>
-            <PersistGate persistor={persistor}>
-                <Router history={history}>
-                    <Routes />
-                    <GlobalStyle />
-                </Router>
-            </PersistGate>
-        </Provider>
+        <BrowserRouter>
+            <Provider store={store}>
+                <PersistGate persistor={persistor}>
+                    <Router history={history}>
+                        <Routes />
+                        <GlobalStyle />
+                    </Router>
+                </PersistGate>
+            </Provider>
+        </BrowserRouter>
     );
 }
 
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -2,7 +2,7 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import api from '../../../services/api';
 import history from '../../../services/history';
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
     try {
